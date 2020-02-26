0bbcb2e0c45d4f745f4f89bc63e2c095c965fa4c
ronnieotieno <ronnieotieno2@gmail.com>
2020-02-26 16:56:18 +0300

    Initial

---
+++ app/src/main/java/dev/ronnie/interviewquestions/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.ronnie.interviewquestions
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val mProgress = circularProgressbar
+//        mProgress.progress = 70
+//        mProgress.secondaryProgress = 100
+//        mProgress.max = 100
+
+    }
+}
